884dca10cc0d0b1d2edad8ad1157c9dd017f0d51
Ángela Teposte <ada_hack184@hackademy.mx>
2022-05-02 20:40:45 -0700

    Primera version modelos
    Ya tiene los campos llenos

--- api/solicitantes/models.py
+++ api/solicitantes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
 class InfoPesonalModel(models.Model):
@@ -6,13 +7,28 @@ class InfoPesonalModel(models.Model):
     middle_name = models.CharField(max_length=30)
     paternal_lastname = models.CharField(max_length=30)
     maternal_lastname = models.CharField(max_length=30)
-    date_birth = 
-    age = 
-    additional_mail= 
-    marital_status = 
+    date_birth = models.DateTimeField ()
+    age = models.PositiveSmallIntegerField(default=29, validators=[MinValueValidator(16), MaxValueValidator(100)])
+    additional_mail = models.CharField(max_length=30) ####CHECAR
+
+    class Gender_List(models.Choices):
+        FEMENINO = "femenino"
+        MASCULINO = "masculino"
+        OTRO = "otro"
+        SINESPECIFICAR = "sin especificar"
+    gender = models.CharField(max_length=20, choices=Gender_List.choices, default='sin especificar')
+
+    class Marital_List(models.Choices):
+        SOLTERO = "soltero"
+        CASADO = "casado"
+        OTRO = "otro"
+        SINESPECIFICAR = "sin especificar"
+    marital_status = models.CharField(max_length=20, choices=Marital_List.choices, default='sin especificar')
+
+   
 
 
 
-      def __str__(self):
-         return self.name
+    def __str__(self): 
+        return self.name
     
