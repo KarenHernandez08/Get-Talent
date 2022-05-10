96785ba6398f20543fc346c0dcae80b706ad99b1
Ángela Teposte <ada_hack184@hackademy.mx>
2022-05-09 22:14:07 -0700

    Resuelto Error Despliegue
    ACA ya esta todo corriendo y se resolvio lo del error del drf-yasg .

--- api/core/settings/base.py
+++ api/core/settings/base.py
@@ -180,7 +180,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Simplified static file serving for Python web apps
 # https://whitenoise.evans.io/en/stable/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Default primary key field type
