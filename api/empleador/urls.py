4d841cb966242dd59130197b1c241330be2b86af
Ángela Teposte <ada_hack184@hackademy.mx>
2022-05-14 11:07:24 -0700

    Cambio en urls empleador
    ups! habia olvidado descomentar el link de Floren

--- api/empleador/urls.py
+++ api/empleador/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from empleador.views import InfoEmpleadorPostView
 
 urlpatterns = [ 
-    path('user_company/', InfoEmpleadorPostView.as_view()),
-    #path('user_company/<int:user_id>/', InfoEmpleadorPostView.as_view()),
+    #path('user_company/', InfoEmpleadorPostView.as_view()),
+    path('user_company/<int:usuario_id>/', InfoEmpleadorPostView.as_view()),
 ]
