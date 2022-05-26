7b9bb4e49ad18ca49795272d0e01602a72e0efc7
Ángela Teposte <ada_hack184@hackademy.mx>
2022-05-26 16:59:34 -0700

    Buenas practicas

--- api/solicitantes/views.py
+++ api/solicitantes/views.py
@@ -12,12 +12,13 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
-from solicitantes.models import VideoSolicitanteModel
-
-from solicitantes.models import InfoPesonalModel #para poder crear los 
-
 from .renderers import SolicitantesRenderer
 from users.models import User
+
+from solicitantes.models import (
+    InfoPesonalModel,
+) 
+
 from solicitantes.serializer import (
     InfoPersonalSerializer,
     VideoSolicitanteSerializer,
