62baedd28ca3b8f74713b79064fcef5347119749
PriscilaIP <ada_hack439@hackademy.mx>
2022-05-26 13:58:41 -0500

    url videosolicitante

--- api/solicitantes/urls.py
+++ api/solicitantes/urls.py
@@ -6,8 +6,8 @@ from solicitantes.views import (
 
 urlpatterns = [
     path('users/<int:usuario_id>/', InfoPersonalRegistroView.as_view()),
-    path('videosolicitante/', VideoSolicitanteView.as_view()),
-    #path('videosolicitante/<int:usuario_id>/', VideoSolicitanteView.as_view()),
+   
+ 
     ##La linea se cambiará cuando se tenga ya el registro de los usuarios
     path('users/videosolicitante', VideoSolicitanteView.as_view()),
 ] 
