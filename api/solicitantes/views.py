315df8bfe25ee1672f1be948ee721e6ada76d08f
KarenHernadez08 <ada_hack109@hackademy.mx>
2022-06-09 12:56:21 -0500

    solicitantes

--- api/solicitantes/views.py
+++ api/solicitantes/views.py
@@ -12,16 +12,18 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from solicitantes.models import InfoPesonalModel
+from solicitantes.models import InfoAcademicaModel
+from solicitantes.models import VideoSolicitanteModel
+
+
 from .renderers import SolicitantesRenderer
 from users.models import User
-
-from solicitantes.models import (
-    InfoPesonalModel,
-) 
-
 from solicitantes.serializer import (
     InfoPersonalSerializer,
     VideoSolicitanteSerializer,
+    InfoAcademicaSerializer
 )
 
 # Create your views here.
@@ -42,7 +44,7 @@ class InfoPersonalRegistroView(generics.GenericAPIView):
                 serializer = InfoPersonalSerializer(data=data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response('Autorización de Solicitante Exitosa. Información de Usuario Registrada', status=status.HTTP_201_CREATED)
+            return Response(' Autorización de Solicitante Exitosa. Información de Usuario Registrada', status=status.HTTP_201_CREATED)
         except:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -59,10 +61,32 @@ class VideoSolicitanteView(generics.GenericAPIView):
             if es_empleador == True:
                 return Response('Eres Empleador. No tienes autorización.', status=status.HTTP_401_UNAUTHORIZED)
             elif es_empleador == False:
-                 serializer = VideoSolicitanteSerializer(data=request.data)
+                return Response('Autorización de Solicitante exitosa. Video Guardado', status=status.HTTP_200_OK)
+            serializer = VideoSolicitanteSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response('Autorización de Solicitante exitosa. Video Guardado', status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class InfoAcademicaView(generics.GenericAPIView): 
+    permission_classes = [permissions.AllowAny]
+    renderer_classes = (SolicitantesRenderer,)
+    queryset = InfoAcademicaModel.objects.all() 
+    serializer_class = InfoAcademicaSerializer
+    def post(self, request, usuario_id):
+        usuario_instance = User.objects.get(id=usuario_id)
+        es_empleador = usuario_instance.is_empleador
+        try:
+            data =request.data
+            serializer = InfoAcademicaSerializer(data=data)
+            if es_empleador == True:
+                return Response('Eres Empleador. No tienes autorización.', status=status.HTTP_401_UNAUTHORIZED)
+            elif es_empleador == False:
+                serializer = InfoAcademicaSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response('Datos guardados correctamente', status=status.HTTP_201_CREATED)
         except:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
