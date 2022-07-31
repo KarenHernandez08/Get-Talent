5e48836e3cb2b63843bf9b3a8a4de2c4c482dbc7
KarenHernadez08 <ada_hack109@hackademy.mx>
2022-07-31 14:20:39 -0500

    correccion de serializers

--- api/postulaciones/serializers.py
+++ api/postulaciones/serializers.py
@@ -5,8 +5,7 @@ from .models import *
 
 
 class PostulacionesSerializer (serializers.ModelSerializer):
-    user_id = serializers.PrimaryKeyRelatedField(write_only = True, queryset = User.objects.all())
-    vacante_id=serializers.PrimaryKeyRelatedField(write_only = True, queryset = VacantesModel.objects.all())
+    
     
     class Meta:
         model = Postula
